288b085e038aa234fdc2626b746c6ea75b433d9d
Erik Pearson <emp823@gmail.com>
2016-08-03 19:36:51 -0400

    Fix indentation

--- lib/generators/data_migrate.rb
+++ lib/generators/data_migrate.rb
@@ -3,7 +3,7 @@ module DataMigrate
   module Generators
     class DataMigrationGenerator < Rails::Generators::NamedBase #:nodoc:
       def self.source_root
-         @_data_migrate_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), generator_name, 'templates'))
+        @_data_migrate_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), generator_name, 'templates'))
       end
     end
   end
